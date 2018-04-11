07bbecb46bc29e1b23bb922ff586ac8434d32e6d
Jonas Martins <jns.martins@outlook.com>
2018-04-11 13:22:44 -0300

    edit

--- Jns.Licenca/TipoDeLicenca.cs
+++ Jns.Licenca/TipoDeLicenca.cs
@@ -9,5 +9,7 @@ namespace Jns.Licenca
     public class TipoDeLicenca
     {
         public string TRIAL { get; set; }
+        public string AVALIACAO { get; set; }
+        public string LIBERADA { get; set; }
     }
 }
